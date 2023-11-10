75700ceb092ddc983cb4c4dd7fc4a93379be0f12
Jacob <steljaco@isu.edu>
2023-11-09 19:02:52 -0700

    working test endpoint

--- highrollers_flask/app.py
+++ highrollers_flask/app.py
@@ -1,6 +1,6 @@
 import flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from highrollers_flask.GameManager import GameManager
 
 app = Flask(__name__)
@@ -11,6 +11,7 @@ game_manager = GameManager()
 def view_home():
     return render_template('index.html')
 
-@app.route('api/test/')
-def test(e):
-    return game_manager.handle_client_message(e)
+@app.route('/api/test/', methods=['POST'])
+def test():
+    return game_manager.handle_client_message(request, "test")
+
