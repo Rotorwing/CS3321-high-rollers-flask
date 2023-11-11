e6183ddf2be3e0e004b03a018aaca70865227dc4
Dakota DeAnda <122389461+SlurmpyDurmpy@users.noreply.github.com>
2023-11-11 15:20:00 -0700

    Add files via upload

--- highrollers_flask/static/script.js
+++ highrollers_flask/static/script.js
@@ -1,105 +1,110 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const gamePlaceholder = document.getElementById("game-placeholder");
-    const resultsContainer = document.createElement("div");
-    resultsContainer.style.display = "flex";
-    resultsContainer.style.flexWrap = "wrap";
-    resultsContainer.style.maxWidth = "600px";
-    resultsContainer.style.justifyContent = "center";
+let resultsContainer;
 
-    // Get a reference to the "Generate Random Card" button and add a click event listener
-    const cardButton = document.getElementById("generateRandomCard");
-    cardButton.addEventListener("click", () => {
-      const numberOfCards = 5;
+document.addEventListener("DOMContentLoaded", function(){
+  resultsContainer = document.getElementById("game-placeholder");
 
-      // Clear the results container
-      resultsContainer.innerHTML = "";
+  const drawCardButton = document.getElementById("drawCardButton");
+  drawCardButton.addEventListener("click", function() {
+    drawCard(false); // Ensure this is false for player cards
+  });
 
-      for (let i = 0; i < numberOfCards; i++) {
-        // Generate a random card for each slot
-        const cardTypes = ["Hearts", "Diamonds", "Clubs", "Spades"];
-        const cardType = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-        let cardNumber;
-        let cardName;
+  newGame();
 
-        if (Math.random() < 0.5) {
-          // Generate a numbered card (e.g., "Clubs_seven")
-          const phoneticNumbers = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
-          cardNumber = phoneticNumbers[Math.floor(Math.random() * phoneticNumbers.length)];
-          cardName = `${cardType}_${cardNumber}`;
-        } else {
-          // Generate a face card (e.g., "Spades_jack")
-          const faceCards = ["ace", "jack", "queen", "king"];
-          const selectedFaceCard = faceCards[Math.floor(Math.random() * faceCards.length)];
-          cardName = `${cardType}_${selectedFaceCard}`;
+  function sendAPICall(data, callback){
+    const Http = new XMLHttpRequest();
+    const url='/api/test/';
+    Http.open("POST", url);
+    Http.setRequestHeader("Content-Type", "application/json");
+  
+    this.callback = callback;
+    Http.onreadystatechange = ((e) => {
+        if(Http.readyState === XMLHttpRequest.DONE){
+            const json = JSON.parse(Http.responseText);
+            this.callback(json);
         }
+    }).bind(this);
+    console.debug(data)
+    let jsonData = JSON.stringify(data);
+    Http.send(jsonData);
+  }
+  
+  window.gameID = ""
+  function newGame(){
+    let data = {
+        "data": "newgame"
+    };
+    sendAPICall(data, (response) => {
+        console.debug(response);
+        if (response && response.id) {
+            window.gameID = response.id;
+            // Clear all previous cards
+            document.getElementById("dealer-cards").innerHTML = "";
+            document.getElementById("player-cards").innerHTML = "";
 
-        // Create an image element for each card
-        const cardImage = document.createElement("img");
-        cardImage.src = "static/images/cards/" + cardName + ".png";
-        cardImage.style.width = "100px";
-        cardImage.style.margin = "5px";
-
-        // Append the card image to the results container
-        resultsContainer.appendChild(cardImage);
-      }
+            // Draw two cards for the dealer
+            drawCard(true); // Draw first card for the dealer
+            drawCard(true); // Draw second card for the dealer
+        }
     });
-
-    // Get a reference to the "Roll the Dice" button and add a click event listener
-    const diceButton = document.getElementById("generateRandomDice");
-    diceButton.addEventListener("click", () => {
-      const numberOfDice = 2;
-
-      // Clear the results container
-      resultsContainer.innerHTML = "";
-
-      for (let i = 0; i < numberOfDice; i++) {
-        // Generate a random dice
-        const phoneticDiceNumbers = ["one", "two", "three", "four", "five", "six"];
-        const diceRoll = phoneticDiceNumbers[Math.floor(Math.random() * phoneticDiceNumbers.length)];
-
-        // Create an image element for the dice
-        const diceImage = document.createElement("img");
-        diceImage.src = "static/images/dice/Dice_" + diceRoll + ".png";
-        diceImage.style.width = "100px";
-        diceImage.style.margin = "5px";
-
-        // Append the dice image to the results container
-        resultsContainer.appendChild(diceImage);
-      }
+  }
+  
+  function drawCard(isDealerCard){
+    let data = {
+        "id": window.gameID,
+        "data": { "action": "draw" }
+    };
+    sendAPICall(data, (response) => {
+        console.debug(response);
+        if (response && response.data && response.data.card) {
+            displayCard(response.data.card, isDealerCard);
+        }
     });
+  }
+  
+  function convertCardName(backendCardName) {
+    const suits = { 'H': 'Hearts', 'D': 'Diamonds', 'C': 'Clubs', 'S': 'Spades' };
+    const values = {
+        'A': 'ace', '2': 'two', '3': 'three', '4': 'four', '5': 'five',
+        '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine',
+        '10': 'ten', 'J': 'jack', 'Q': 'queen', 'K': 'king'
+    };
 
-    // Append the results container to the gamePlaceholder
-    gamePlaceholder.appendChild(resultsContainer);
+    let value = backendCardName.slice(0, -1);
+    let suit = backendCardName.slice(-1);
 
-    // ---------------------------TESTING--------------------------- //
-
-    // Points tracker
-    let points = 0; // Initialize points
-    const pointsContainer = document.getElementById('points-placeholder'); // Get the existing div from the HTML
-    const pointsDisplay = document.createElement('div'); // Create a new div for displaying points
-    pointsDisplay.id = 'pointsDisplay';
-    pointsDisplay.textContent = `Points: ${points}`;
-    pointsContainer.appendChild(pointsDisplay); // Append the new points display div
-
-    // Get references to buttons from the HTML
-    const incrementButton = document.getElementById('incrementPoints');
-    const decrementButton = document.getElementById('decrementPoints');
-
-    // Function to update points display
-    function updatePointsDisplay() {
-        pointsDisplay.textContent = `Points: ${points}`; // Update text content of the new points display div
+    if (backendCardName == "B")
+    {
+      return "Card_back.png";
     }
 
-    // Add event listeners to buttons
-    incrementButton.addEventListener('click', () => {
-        points += 5; // Increment points by 5
-        updatePointsDisplay();
-    });
+    if (value === '1') {
+        value = '10';
+    }
 
-    decrementButton.addEventListener('click', () => {
-        points = Math.max(0, points - 5); // Decrement points by 5 but not below 0
-        updatePointsDisplay();
-    });
+    let fullSuitName = suits[suit];
+    let fullValueName = values[value];
 
-    // ---------------------------TESTING--------------------------- //
+    return `${fullSuitName}_${fullValueName}.png`;
+  }
+
+  // Modify this function to accept a second parameter
+  function displayCard(backendCardName, isDealerCard) {
+    let cardFilename = convertCardName(backendCardName);
+    const cardImage = document.createElement("img");
+    cardImage.src = "static/images/cards/" + cardFilename;
+    cardImage.style.width = "100px";
+
+    let targetContainerId = isDealerCard ? "dealer-cards" : "player-cards";
+    let targetContainer = document.getElementById(targetContainerId);
+
+    console.log("Target container ID:", targetContainerId);
+    console.log("Target container element:", targetContainer);
+
+    if (!targetContainer) {
+        console.error("Element with ID '" + targetContainerId + "' not found.");
+        return;
+    }
+
+    targetContainer.appendChild(cardImage);
+  }
 });
