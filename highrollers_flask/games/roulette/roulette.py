d97b990db3167832ac1e37b84b5defc7b8195448
Luna Steed <lunasteed@isu.edu>
2023-11-28 17:28:04 -0700

    Added super().__init__ to Roulette
    Added super().__init__ for standardization and ensuring the communication works

--- highrollers_flask/games/roulette/roulette.py
+++ highrollers_flask/games/roulette/roulette.py
@@ -7,7 +7,8 @@ Contains the logic for the game of Roulette.
 """
 
 class Roulette:
-    def __init__(self):
+    def __init__(self, manager) -> None:
+        super().__init__(manager)
         """Initializing Function"""
         self.numsDict = {"red": [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 36],
                          "black": [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
