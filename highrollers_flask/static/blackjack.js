79229eeb43b5f28e5ea1271a1ede24580e2573af
Dakota DeAnda <122389461+SlurmpyDurmpy@users.noreply.github.com>
2023-12-03 14:53:21 -0700

    Add files via upload

--- highrollers_flask/static/blackjack.js
+++ highrollers_flask/static/blackjack.js
@@ -5,27 +5,77 @@ function newGame() {
         "data": "newgame"
     };
     sendAPICall(data, (response) => {
-        console.debug(response);
+        console.debug("Response:", response); // Debugging statement to log the response
+
+        // Check if the response contains an 'id'
         if (response && response.id) {
             gameID = response.id;
+
+            // Clear the HTML elements for dealer and player cards
             document.getElementById("dealer-cards").innerHTML = "";
             document.getElementById("player-cards").innerHTML = "";
-            drawCard(true); // Draw first card for the dealer
-            drawCard(true); // Draw second card for the dealer
+
+            // Check if dealer cards exist in the response
+            if (response.dealer && Array.isArray(response.dealer) && response.dealer.length > 0) {
+                // Draw the dealer's cards
+                response.dealer.forEach((card) => {
+                    displayCard(card, true);
+                });
+            } else {
+                console.error("Dealer cards not found in the response:", response);
+                // Handle the error or log the response for debugging
+            }
+
+            // Check if player cards exist in the response and draw them
+            if (response.player && Array.isArray(response.player) && response.player.length > 0) {
+                response.player.forEach((card) => {
+                    displayCard(card, false);
+                });
+            }
+        } else {
+            console.error("No 'id' found in the response:", response);
+            // Handle the error or log the response for debugging
         }
     });
 }
 
-function drawCard(isDealerCard) {
+function playerHit() {
     let data = {
         "id": gameID,
-        "data": { "action": "draw" }
+        "data": { "action": "hit" }
     };
     sendAPICall(data, (response) => {
         console.debug(response);
-        if (response && response.data && response.data.card) {
-            displayCard(response.data.card, isDealerCard);
+        if (response && response.card) {
+            displayCard(response.card, false); // false for player card
         }
+        // Additional UI updates if necessary
+    });
+}
+
+function dealerHit() {
+    let data = {
+        "id": gameID,
+        "data": { "action": "hit" }
+    };
+    sendAPICall(data, (response) => {
+        console.debug(response);
+        if (response && response.card) {
+            displayCard(response.card, true); // true for dealer card
+        }
+        // Additional UI updates if necessary
+    });
+}
+
+function playerStand() {
+    let data = {
+        "id": gameID,
+        "data": { "action": "stand" }
+    };
+    sendAPICall(data, (response) => {
+        console.debug(response);
+        // Handle the response for the stand action
+        // Update the UI accordingly
     });
 }
 
@@ -36,10 +86,6 @@ function displayCard(backendCardName, isDealerCard) {
     cardImage.style.width = "100px";
     let targetContainerId = isDealerCard ? "dealer-cards" : "player-cards";
     let targetContainer = document.getElementById(targetContainerId);
-    if (!targetContainer) {
-        console.error("Element with ID '" + targetContainerId + "' not found.");
-        return;
-    }
     targetContainer.appendChild(cardImage);
 }
 
@@ -64,8 +110,12 @@ function convertCardName(backendCardName) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    const drawCardButton = document.getElementById("drawCardButton");
-    drawCardButton.addEventListener("click", function() {
-        drawCard(false); // Ensure this is false for player cards
-    });
+    const hitButton = document.getElementById("hitButton");
+    const standButton = document.getElementById("standButton");
+
+    hitButton.addEventListener("click", playerHit);
+    standButton.addEventListener("click", playerStand);
+
+    // Initialize the game
+    newGame();
 });
