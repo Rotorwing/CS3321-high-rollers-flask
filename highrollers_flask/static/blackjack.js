828ce5257dedbdd2a4fff2900f7c7cc744243ea3
Dakota DeAnda <122389461+SlurmpyDurmpy@users.noreply.github.com>
2023-12-04 20:26:05 -0700

    Add files via upload

--- highrollers_flask/static/blackjack.js
+++ highrollers_flask/static/blackjack.js
@@ -1,166 +1,118 @@
 let gameID = "";
 
 function newGame() {
-    let data = { "data": "newgame" };
-    sendAPICall(data, (response) => {
-        console.debug(response);
+    sendAPICall({ "data": "newgame" }, (response) => {
         if (response && response.id) {
             gameID = response.id;
-
-            // Clear the HTML elements for dealer and player cards
             document.getElementById("dealer-cards").innerHTML = "";
             document.getElementById("player-cards").innerHTML = "";
-
-            // Display dealer cards
-            if (response.data.dealer && Array.isArray(response.data.dealer)) {
-                response.data.dealer.forEach((card) => {
-                    displayCard(card, true);
-                });
-            }
-
-            // Display player cards
-            if (response.data.player && Array.isArray(response.data.player)) {
-                response.data.player.forEach((card) => {
-                    displayCard(card, false);
-                });
-            }
-
-            // Additional UI updates if necessary
+            response.data.dealer.forEach(card => displayCard(card, true));
+            response.data.player.forEach(card => displayCard(card, false));
         }
     });
+    hideNewGameButton();
+    enableButtons(); 
+}
+
+
+function showNewGameButton() {
+    document.getElementById("newGameButton").style.display = "inline-block";
+}
+
+function hideNewGameButton() {
+    document.getElementById("newGameButton").style.display = "none";
+}
+
+function disableButtons() {
+    document.getElementById("hitButton").disabled = true;
+    document.getElementById("standButton").disabled = true;
+}
+
+function enableButtons() {
+    document.getElementById("hitButton").disabled = false;
+    document.getElementById("standButton").disabled = false;
 }
 
 function playerHit() {
-    let data = {
-        "id": gameID,
-        "data": { "action": "hit" }
-    };
-    sendAPICall(data, (response) => {
-        console.debug(response);
-        if (response && response.card) {
-            displayCard(response.card, false); // false for player card
-            
-            // Check for player bust
-            if (response.player_value > 21) {
-                console.log("Player bust!"); // Add a console message for debugging
-                // Send a message to the server indicating player bust
-                sendBustMessage("player");
-                // Additional UI updates if necessary
-            }
+    sendAPICall({ "id": gameID, "data": { "action": "hit" } }, (response) => {
+        if (response === "dealer") {
+            updateGameStatus("Player Busts!");
+            disableButtons();
+            showNewGameButton();
+        } else if (response.card) {
+            displayCard(response.card, false);
         }
-    });
-}
-
-function dealerHit() {
-    let data = {
-        "id": gameID,
-        "data": { "action": "hit" }
-    };
-    sendAPICall(data, (response) => {
-        console.debug(response);
-        if (response && response.card) {
-            displayCard(response.card, true); // true for dealer card
-            
-            // Check for dealer bust
-            if (response.dealer_value > 21) {
-                console.log("Dealer bust!"); // Add a console message for debugging
-                // Send a message to the server indicating dealer bust
-                sendBustMessage("dealer");
-                // Additional UI updates if necessary
-            }
-        }
-    });
-}
-
-function sendBustMessage(player) {
-    // Send a message to the server indicating the player (player or dealer) has busted
-    let data = {
-        "id": gameID,
-        "data": { "action": "bust", "player": player }
-    };
-    sendAPICall(data, (response) => {
-        // Handle the server response if needed
     });
 }
 
 function playerStand() {
-    let data = {
-        "id": gameID,
-        "data": { "action": "stand" }
-    };
-    sendAPICall(data, (response) => {
-        console.debug(response);
-        // Handle the response for the stand action
-        // Update the UI accordingly
+    disableButtons();
+    sendAPICall({ "id": gameID, "data": { "action": "stand" } }, (response) => {
+        if (response) {
+            if (response.dealer) {
+                document.getElementById("dealer-cards").innerHTML = "";
+                response.dealer.forEach(card => displayCard(card, true));
+                if (response.result === "player") {
+                    updateGameStatus("Player Wins!");
+                } else if (response.result === "dealer") {
+                    updateGameStatus("Dealer Wins!");
+                } else {
+                    updateGameStatus("Tie!");
+                }
+                showNewGameButton();
+            } else {
+                console.error("Unexpected response format from server:", response);
+            }
+        } else {
+            updateGameStatus("Error: No response from server");
+            showNewGameButton();
+        }
     });
 }
 
-function calculateHandValue(cards) {
-    let handValue = 0;
-    let aceCount = 0;
-
-    // Iterate through the cards and calculate the hand value
-    for (const card of cards) {
-        const value = card.slice(0, -1);
-        if (value === 'A') {
-            // Handle Ace separately, as it can be 1 or 11
-            aceCount++;
-            handValue += 11;
-        } else if (value === 'K' || value === 'Q' || value === 'J') {
-            // Face cards are worth 10 points
-            handValue += 10;
-        } else {
-            // Numeric cards are worth their face value
-            handValue += parseInt(value);
-        }
-    }
-
-    // Adjust the hand value if there are Aces and the total value exceeds 21
-    while (aceCount > 0 && handValue > 21) {
-        handValue -= 10;
-        aceCount--;
-    }
-
-    return handValue;
+function updateGameStatus(message) {
+    const pointsBar = document.getElementById("points-bar");
+    pointsBar.textContent = message;
 }
 
 function displayCard(backendCardName, isDealerCard) {
-    let cardFilename = convertCardName(backendCardName);
+    const cardFilename = convertCardName(backendCardName);
     const cardImage = document.createElement("img");
     cardImage.src = "static/images/cards/" + cardFilename;
     cardImage.style.width = "100px";
-    let targetContainerId = isDealerCard ? "dealer-cards" : "player-cards";
-    let targetContainer = document.getElementById(targetContainerId);
-    targetContainer.appendChild(cardImage);
+    document.getElementById(isDealerCard ? "dealer-cards" : "player-cards").appendChild(cardImage);
 }
 
 function convertCardName(backendCardName) {
     const suits = { 'H': 'Hearts', 'D': 'Diamonds', 'C': 'Clubs', 'S': 'Spades' };
-    const values = {
-        'A': 'ace', '2': 'two', '3': 'three', '4': 'four', '5': 'five',
-        '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine',
-        '10': 'ten', 'J': 'jack', 'Q': 'queen', 'K': 'king'
-    };
+    const values = { 'A': 'ace', '2': 'two', '3': 'three', '4': 'four', '5': 'five', '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine', '10': 'ten', 'J': 'jack', 'Q': 'queen', 'K': 'king' };
     let value = backendCardName.slice(0, -1);
     let suit = backendCardName.slice(-1);
-    if (backendCardName == "B") {
-        return "Card_back.png";
-    }
-    if (value === '1') {
-        value = '10';
-    }
-    let fullSuitName = suits[suit];
-    let fullValueName = values[value];
-    return `${fullSuitName}_${fullValueName}.png`;
+    return backendCardName == "B" ? "Card_back.png" : `${suits[suit]}_${values[value === '1' ? '10' : value]}.png`;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     const hitButton = document.getElementById("hitButton");
     const standButton = document.getElementById("standButton");
+    const newGameButton = document.getElementById("newGameButton");
 
-    hitButton.addEventListener("click", playerHit);
-    standButton.addEventListener("click", playerStand);
+    if (hitButton) {
+        hitButton.addEventListener("click", playerHit);
+    } else {
+        console.error("Hit button not found");
+    }
 
-    // Initialize the game
+    if (standButton) {
+        standButton.addEventListener("click", playerStand);
+    } else {
+        console.error("Stand button not found");
+    }
+
+    if (newGameButton) {
+        newGameButton.addEventListener("click", newGame);
+    } else {
+        console.error("New game button not found");
+    }
+
     newGame();
 });
